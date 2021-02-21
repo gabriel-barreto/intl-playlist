c4fac6d972b36c714aca470b3c931901b59fe15c
Gabriel Barreto <barreto-gabriel@outlook.com>
2021-02-21 15:03:07 -0300

    feat: handle with new interface to work with user input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+import chalk from 'chalk'
 import database from '../database.json'
 
 import { TerminalDisplay } from './UI/terminal.ui.js'
 
-function main() {
-  new TerminalDisplay(database).setup().print()
+async function main() {
+  const terminal = new TerminalDisplay(database).setup()
+
+  try {
+    await terminal.input()
+  } catch (ex) {
+    console.log(chalk.red('Ooops...'), '\n', chalk.gray(ex.message))
+  } finally {
+    return terminal.input()
+  }
 }
 
-main()
+await main()
